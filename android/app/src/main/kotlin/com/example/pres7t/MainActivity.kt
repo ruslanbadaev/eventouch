4e84ddb05ae3d19df01f7f5885578372092094f7
Ruslan Badaev <r.badaeff@gmail.com>
2022-06-15 15:00:29 +0700

    feat(google_maps): Added google_maps functionality

--- android/app/src/main/kotlin/com/example/pres7t/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/pres7t/MainActivity.kt
@@ -1,6 +1,9 @@
-package com.example.pres7t
+package com.ruslanbadaev.pres7t
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    // if (defaultTargetPlatform == TargetPlatform.android) {
+    //     AndroidGoogleMapsFlutter.useAndroidViewSurface = true;
+    // }
 }
